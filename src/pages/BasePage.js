3e331a8025150edcddf25a5dc6454ce03344b87b
sr.veremienko@gmail.com <sr.veremienko@gmail.com>
2025-08-18 22:32:01 +0300

    Added oop methods

---
+++ src/pages/BasePage.js
@@ -0,0 +1,39 @@
+const { step } = require('@playwright/test');
+
+export class BasePage {
+  _url;
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  url() {
+    if (this._url) {
+      return this._url;
+    } else {
+      throw Error(`The property '_url' must be implemented`);
+    }
+  }
+
+  _pageName() {
+    return this.constructor.name.replace('Page', '');
+  }
+
+  async open() {
+    await step(`Open ${this._pageName()} page`, async () => {
+      await this.page.goto(this.url());
+    });
+  }
+
+  async waitForLoading() {
+    await step(`Wait for ${this._pageName()} page to open`, async () => {
+      await this.page.waitForURL(this.url());
+    });
+  }
+
+  async reload() {
+    await step(`Reload the Cart Page`, async () => {
+      await this.page.reload();
+    });
+  }
+}
