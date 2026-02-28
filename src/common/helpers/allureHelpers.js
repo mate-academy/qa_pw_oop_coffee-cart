2617c3e67153ebca98bcae24654ea3e4db1003e5
pexepu <pexepu@tutanota.com>
2026-02-28 13:50:19 +0000

    solution

--- src/common/helpers/allureHelpers.js
+++ src/common/helpers/allureHelpers.js
@@ -3,19 +3,25 @@ import { camelCaseToPhrase, capitalize } from './stringHelpers';
 export function parseTestTreeHierarchy(fileName, logger) {
   const testFolder = 'tests/';
 
-  const attributesCamelCase = fileName
-    .substring(fileName.indexOf(testFolder) + testFolder.length)
+  const normalized = String(fileName).replace(/\\/g, '/');
+  const folderIndex = normalized.indexOf(testFolder);
+
+  if (folderIndex < 0) {
+    return [];
+  }
+
+  const attributesCamelCase = normalized
+    .substring(folderIndex + testFolder.length)
     .split('/');
 
   let attributes = attributesCamelCase.map(attribute =>
     capitalize(camelCaseToPhrase(attribute)),
   );
 
-  if (attributes[2].includes('.spec.js')) {
+  if (typeof attributesCamelCase[2] === 'string' && attributesCamelCase[2].includes('.spec.js')) {
     attributes = attributes.slice(0, 2);
   }
 
   logger.debug(`Parsed test hierarchy: ${JSON.stringify(attributes)}`);
-
   return attributes;
 }
---
+++ src/pages/BasePage.js
@@ -0,0 +1,39 @@
+const { expect, step } = require('@playwright/test');
+export class BasePage {
+  _url;
+  constructor(page) {
+    this.page = page;
+  }
+
+
+
+  url() {
+    if (this._url) {
+      return this._url;
+    } else {
+      throw Error(`The property '_url' must be implemented`);
+    }
+  }
+
+  async open() {
+    await step(`Open ${this._pageName()} page`, async () => {
+      await this.page.goto(this.url());
+    });
+  }
+
+  async waitForLoading() {
+    await step(`Wait for ${this._pageName()} to open`, async () => {
+      await this.page.waitForURL(this.url());
+    });
+  }
+
+  async reload() {
+    await step(`Reload the ${this._pageName()} Page`, async () => {
+      await this.page.reload();
+    });
+  }
+
+  _pageName() {
+    return this.constructor.name.replace('Page', '');
+  }
+}
