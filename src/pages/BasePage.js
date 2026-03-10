3c7653b1fb176f7ca3a04884c4d4ac2d40b4dab5
Marianna <marianna@syndeo.cx>
2026-03-11 00:04:10 +0200

    Solution

---
+++ src/pages/BasePage.js
@@ -0,0 +1,44 @@
+import { test } from '@playwright/test';
+
+export class BasePage {
+    _url;
+
+    constructor(page) {
+        this.page = page;
+    }
+
+    async step(title, stepToRun) {
+        return await test.step(title, stepToRun, this.userId);
+    }
+
+    url() {
+        if (this._url) {
+          return this._url;
+        } else {
+          throw Error(`The property '_url' must be implemented`);
+        }
+    }
+
+    _pageName() {
+        return this.constructor.name.replace('Page', '');
+    }
+
+    async open() {
+        await this.step(`Open ${this._pageName()} page`, async () => {
+          await this.page.goto(this.url());
+        });
+    }
+
+    async waitForLoading() {
+        await this.step(
+            `Wait for ${this._pageName()} page to open`, async () => {
+          await this.page.waitForURL(this.url());
+        });
+    }
+    
+    async reload() {
+        await this.step(`Reload the ${this._pageName()} Page`, async () => {
+          await this.page.reload();
+        });
+    }
+}
