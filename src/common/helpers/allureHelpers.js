4282036fff6b53f3e53a14bb447efd2376bb5095
Anton Sialitski <selitskiy.anton@gmail.com>
2026-09-01 11:43:08 +0200

    solution

--- src/common/helpers/allureHelpers.js
+++ src/common/helpers/allureHelpers.js
@@ -2,16 +2,17 @@ import { camelCaseToPhrase, capitalize } from './stringHelpers';
 
 export function parseTestTreeHierarchy(fileName, logger) {
   const testFolder = 'tests/';
+  const normalizedFileName = fileName.replace(/\\/g, '/');
 
-  const attributesCamelCase = fileName
-    .substring(fileName.indexOf(testFolder) + testFolder.length)
+  const attributesCamelCase = normalizedFileName
+    .substring(normalizedFileName.indexOf(testFolder) + testFolder.length)
     .split('/');
 
   let attributes = attributesCamelCase.map(attribute =>
     capitalize(camelCaseToPhrase(attribute)),
   );
 
-  if (attributes[2].includes('.spec.js')) {
+  if (attributes[2] && attributes[2].includes('.spec.js')) {
     attributes = attributes.slice(0, 2);
   }
 
